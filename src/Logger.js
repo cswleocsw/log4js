6fc53a9fb4033813ad7ef837d91b38971618dcfc
cswleocsw <cswleo@gmail.com>
2016-08-24 11:40:50 +0800

    fix eslint

--- src/Logger.js
+++ src/Logger.js
@@ -1,11 +1,21 @@
 export const LEVEL_MAP = { DEBUG: 1, INFO: 2, WARN: 3, ERROR: 4, FATAL: 5 }
 export const LEVEL_KEY = { 1: 'DEBUG', 2: 'INFO', 3: 'WARN', 4: 'ERROR', 5: 'FATAL' }
 
+/**
+ *
+ * @param {string} str - string
+ * @param {number} len - length
+ * @returns {string} - string
+ */
 function padLeft(str, len) {
   const s = `${str}`
-  return (s.length >= len) ? s : padLeft(`0${s}`, len)
+  return s.length >= len ? s : padLeft(`0${s}`, len)
 }
 
+/**
+ *
+ * @returns {string} - date time string
+ */
 function getDateTime() {
   let d = new Date()
   return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${padLeft(d.getHours(), 2)}:${padLeft(d.getMinutes(), 2)}:${padLeft(d.getSeconds(), 2)}:${d.getMilliseconds()}`
